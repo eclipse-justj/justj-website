c974f6d0a5cd98eb22a1ad01bc92154bd5541c9e
Ed Merks <ed.merks@gmail.com>
2020-05-24 06:51:28 +0200

    Bug 562908 - Populate the JustJ Git clones with initial content
    Flesh out more of the website pages.
    
    Validate that this build is automatically triggered by the commit:
    
    https://ci.eclipse.org/justj/job/promote-website/

--- content/developer.php
+++ content/developer.php
@@ -11,8 +11,34 @@
 ?>
 
 <div id="midcolumn">
-  <h1><?php print $eclipse_justj;?></h1>
-  <p>
-  Getting involved under construction.
-  </p>
+<h1><img style="height: 2ex;" src="justj_title.svg" alt="Eclipse JustJ"/> Development</h1>
+
+<h2>Git Repositories</h2>
+<p>
+<code><a href="https://git.eclipse.org/c/justj/justj.tools.git/">justj.tools.git</a></code> - the tools used to generate JREs and to manage the <code><a href="https://download.eclipse.org/justj/">download.eclipse.org</a></code> site.
+</p>
+<p>
+<code><a href="https://git.eclipse.org/c/justj/justj.git/">justj.git</a></code> - the projects used for driving <code><a href="https://ci.eclipse.org/justj/">https://ci.eclipse.org/justj/</a></code>.
+</p>
+<p>
+<code><a href="https://git.eclipse.org/c/www.eclipse.org/justj.git/">www.eclipse.org/justj.tools.git</a></code> - the project used to maintain this website.
+</p>
+
+
+<h2>Builds</h2>
+<p>
+<code><a href="https://ci.eclipse.org/justj/">https://ci.eclipse.org/justj/</a></code>
+</p>
+
+<h2>Wiki</h2>
+<p>
+<code><a href="https://wiki.eclipse.org/JustJ">https://wiki.eclipse.org/JustJ</a></code>
+</p>
+
+<h2>Workspace Setup</h2>
+<p>
+JustJ has a fully automated Oomph setup.
+Helpful details TBD.
+<p>
+
 </div>
--- content/support.php
+++ content/support.php
@@ -11,8 +11,28 @@
 ?>
 
 <div id="midcolumn">
-  <h1><?php print $eclipse_justj;?></h1>
-  <p>
-  Support under construction.
-  </p>
+<h1><img style="height: 2ex;" src="justj_title.svg" alt="Eclipse JustJ"/> Support</h1>
+  
+<h2>Issue Tracking</h2>
+<p>
+Browse the outstanding <code><a href="https://bugs.eclipse.org/bugs/buglist.cgi?bug_status=UNCONFIRMED&bug_status=NEW&bug_status=ASSIGNED&bug_status=REOPENED&classification=Technology&order=Importance&product=Justj&query_format=advanced">JustJ Bugzilla Issues</a></code>.
+</p>
+<p>
+Report a <code><a href="https://bugs.eclipse.org/bugs/enter_bug.cgi?product=Justj">JustJ Bugzilla Issue</a></code>.
+</p>
+
+<h2>Communication</h2>
+<p>
+As a question on the <code><a href="https://www.eclipse.org/forums/index.php/f/532/">JustJ Forum</a></code>.
+</p>
+<p>
+Discuss issues with other project participants on <code><a href="https://accounts.eclipse.org/mailing-list/justj-dev">justj-dev mailing list</a></code>.
+</p>
+
+<h2>Professional Support</h2>
+<p>
+Contact <code>ed.merks at gmail.com</code>.
+He can make anything work and can make anything that already works work better.
+</p>
+  
 </div>

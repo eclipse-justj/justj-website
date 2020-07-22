216395f24b9dfaed03fbf634572b9d5a6d1b35da
Ed Merks <ed.merks@gmail.com>
2020-07-22 09:31:17 +0200

    Bug 562908 - Populate the JustJ Git clones with initial content
    Generate a link to the all releases composite.
    
    Signed-off-by: Ed Merks <ed.merks@gmail.com>

--- content/overview.php
+++ content/overview.php
@@ -14,6 +14,10 @@ foreach ($jres as $jre) {
 
 echo "<h3 id='p2-update-sites'>JRE p2 Update Sites</h3>";
 
+if (is_file("$jres_folder/index.html")) {
+    echo "<code><a href='https://download.eclipse.org/justj/sandbox/jres/'>JRE All Releases Composite</a></code><br/>";
+}
+
 foreach ($jres as $jre) {
   if (is_dir("$jres_folder/$jre/updates")) {
     echo "<code><a href='https://download.eclipse.org/justj/sandbox/jres/$jre/updates/'>JRE $jre Updates</a></code><br/>";
